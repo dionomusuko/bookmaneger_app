b52d629da88241a80abe0ff251705e952ee1d16f
dionomusuko <dandybird1017@gmail.com>
2019-06-12 00:04:02 +0900

    bm1 ver.2 comp

--- bm1.rb
+++ bm1.rb
@@ -14,7 +14,7 @@ server.mount_proc "/" do |req, res|
     names = []
     dbh = DBI.connect('DBI:SQLite3:names.db')
         dbh.select_all("select * from bookt") do |row|
-        names << {title: row["title"], editor: row["editor"], pages: row["pages"], day: row["day"]}
+        names << {id: row["id"],title: row["title"], editor: row["editor"], pages: row["pages"], day: row["day"]}
         end
     dbh.disconnect
    res.body = ERB.new(File.read('bm1.erb')).result(binding)
@@ -26,6 +26,33 @@ server.mount_proc "/" do |req, res|
     dbh.disconnect
     res.set_redirect(WEBrick::HTTPStatus::TemporaryRedirect, '/')
    end
+   
+   server.mount_proc "/bookinfo/edit" do |req, res|
+    id = req.query["id"]
+    title = ""
+    editor = ""
+    pages = ""
+    day = ""
+    dbh = DBI.connect('DBI:SQLite3:names.db')
+        dbh.select_all("select * from bookt where id=#{id}") do |row|
+        name = row[0]
+    end
+    res.body = ERB.new(File.read('edit.erb')).result(binding)
+    end
+
+    server.mount_proc "/bookinfo/delete" do |req, res|
+        dbh = DBI.connect('DBI:SQLite3:names.db')
+        dbh.do("delete from bookt where id=#{req.query["id"]}")
+        dbh.disconnect
+        res.set_redirect(WEBrick::HTTPStatus::TemporaryRedirect, '/')
+    end
+    
+    server.mount_proc "/bookinfo/update" do |req, res|
+        dbh = DBI.connect('DBI:SQLite3:names.db')
+        dbh.do("update bookt set title = ('#{req.query["title"]}'), editor = ('#{req.query["editor"]}'), pages = #{req.query["pages"]}, day = ('#{req.query["day"]}') where id = #{req.query["id"]}")
+        dbh.disconnect
+        res.set_redirect(WEBrick::HTTPStatus::TemporaryRedirect, '/')
+    end
 
 trap(:INT) do 
     server.shutdown
